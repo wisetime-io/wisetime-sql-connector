bf25ad01331522738f2d146f386b989d8b3f46dc
vadym <vadym.sukolen@staff.wisetime.com>
2019-08-02 12:04:21 +0800

    WT-7915 integration configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,21 @@
 rootProject.name = "wisetime-sql-connector"
+
+pluginManagement {
+
+    resolutionStrategy.eachPlugin {
+        if (requested.id.id == "com.google.protobuf") {
+            useModule("com.google.protobuf:protobuf-gradle-plugin:0.8.9")
+        }
+    }
+
+    repositories {
+        gradlePluginPortal()
+        google()
+        maven {
+            setUrl("https://s3.eu-central-1.amazonaws.com/artifacts.wisetime.com/mvn2/plugins")
+            content {
+                includeGroup("io.wisetime.versionChecker")
+            }
+        }
+    }
+}
