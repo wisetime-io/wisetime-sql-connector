0aee98f6346a88438a4bc055275e35659f6209b6
Dusan Grubjesic <grubjesic.dusan@gmail.com>
2019-08-23 12:34:03 +0200

    feature/WT-8084: fix version check

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ repositories {
 }
 
 dependencies {
-    implementation("io.wisetime:wisetime-connector:2.2.2-2")
+    implementation("io.wisetime:wisetime-connector:2.2.3")
     implementation("org.apache.commons:commons-configuration2:2.5")
     implementation("com.google.guava:guava:28.0-jre")
 
