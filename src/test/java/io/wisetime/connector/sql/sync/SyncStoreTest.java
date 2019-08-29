a64ceaa86302db3aa36ce1c5f50cec088f93b147
Dusan Grubjesic <grubjesic.dusan@gmail.com>
2019-08-29 13:59:57 +0200

    feature/WT-8084: update version/imports

--- src/test/java/io/wisetime/connector/sql/sync/SyncStoreTest.java
+++ src/test/java/io/wisetime/connector/sql/sync/SyncStoreTest.java
@@ -9,7 +9,6 @@ import static io.wisetime.connector.sql.RandomEntities.fixedTimeMinusMinutes;
 import static io.wisetime.connector.sql.RandomEntities.randomTagSyncRecord;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
