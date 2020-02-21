e1fc364ecdb4429c5cc1521702b8181aa6788d91
Vy-Shane Xie <shane@node.mu>
2020-02-21 09:55:27 +0800

    Update slf4j dependency to match connector library

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
 
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("ch.qos.logback:logback-core:1.2.3")
-    implementation("org.slf4j:slf4j-api:1.7.26")
+    implementation("org.slf4j:slf4j-api:1.7.30")
 
     implementation("org.codejargon:fluentjdbc:1.8.3")
     implementation("com.zaxxer:HikariCP:3.3.1")
