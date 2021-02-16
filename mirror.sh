95ed7b1e68b119c78737916b3afb548fe59453fe
Sumit Patel <sumit.patel@practiceinsight.io>
2021-02-16 09:23:18 +0530

    updated mirror script

--- mirror.sh
+++ mirror.sh
@@ -2,8 +2,12 @@
 set -o errexit ; set -o errtrace ; set -o pipefail
 
 git fetch --tags
-git checkout master
-rm -rf bamboo-specs
+git checkout --orphan temp_branch
+rm -rf bamboo-specs mirror.sh
+git add -A
+git commit -am "Mirror Repo"
+git branch -D master
+git branch -m master
 mkdir -p ~/.ssh
 # Ensure we can talk to GitHub
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
@@ -12,6 +16,6 @@ echo "${GITHUB_SSH_KEY_B64}" | base64 -d > /tmp/.ssh/github.key
 chmod 600 /tmp/.ssh/github.key
 # Push to GitHub mirror
 git remote add github git@github.com:wisetime-io/wisetime-sql-connector.git
-GIT_SSH_COMMAND='ssh -i /tmp/.ssh/github.key' git push github master --tags
+GIT_SSH_COMMAND='ssh -i /tmp/.ssh/github.key' git push -f github master --tags
 rm -rf /tmp/.ssh
 echo "Push to GitHub mirror complete"
