8fc9157a84d392f25f703be3317c60da477b65ac
Sumit Patel <sumit.patel@practiceinsight.io>
2021-02-16 10:17:26 +0530

    updated git name

--- mirror.sh
+++ mirror.sh
@@ -2,7 +2,7 @@
 set -o errexit ; set -o errtrace ; set -o pipefail
 
 git config --global user.email "devops@wisetime.com"
-git config --global user.name "WiseTime Mirror Bot"
+git config --global user.name "WiseTime Bot"
 git fetch --tags
 git checkout --orphan temp_branch
 rm -rf bamboo-specs mirror.sh
