ff2d78410bcb4edea40d806e87b2fd915f432e75
Thomas Haines <thomas.haines@gmail.com>
2024-11-25 01:17:13 +0000

    Mirror Repo

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+rootProject.name = "wisetime-sql-connector"
+
+pluginManagement {
+
+  plugins {
+    id("io.wisetime.versionChecker") version "10.13.27"
+  }
+
+  resolutionStrategy.eachPlugin {
+    if (requested.id.id == "com.google.protobuf") {
+      useModule("com.google.protobuf:protobuf-gradle-plugin:0.8.9")
+    }
+  }
+
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+    maven {
+      setUrl("https://s3.eu-central-1.amazonaws.com/artifacts.wisetime.com/mvn2/plugins")
+      content {
+        includeGroup("io.wisetime.versionChecker")
+      }
+    }
+  }
+}
