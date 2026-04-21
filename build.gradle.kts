a1222771ab33ddddc453cb18a1694b86c93e6292
Thomas Haines <thaines@anaqua.com>
2026-04-21 01:22:34 +0000

    Mirror Repo

---
+++ build.gradle.kts
@@ -0,0 +1,196 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import io.wisetime.version.model.LegebuildConst
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+  application
+  checkstyle
+  idea
+  jacoco
+  java
+  `maven-publish`
+  id("com.google.cloud.tools.jib") version "3.4.5"
+  id("io.freefair.lombok") version "9.1.0"
+  id("io.wisetime.versionChecker")
+  id("fr.brouillard.oss.gradle.jgitver").version("0.9.1")
+  id("com.github.ben-manes.versions").version("0.51.0")
+}
+
+repositories {
+  mavenCentral()
+
+  maven {
+    // Our published releases repo
+    url = uri("https://europe-west3-maven.pkg.dev/wise-pub/java")
+    content {
+      includeGroup("io.wisetime")
+    }
+  }
+}
+
+dependencies {
+  implementation("io.wisetime:wisetime-connector:5.1.91")
+  implementation("org.apache.httpcomponents:httpcore:4.4.16")
+  implementation("commons-codec:commons-codec:1.17.2")
+  implementation("io.vavr:vavr:0.10.5")
+  implementation("org.apache.commons:commons-configuration2:2.14.0") {
+    exclude("commons-logging")
+  }
+  implementation("com.google.guava:guava:${LegebuildConst.GUAVA_VERSION}")
+  implementation("com.google.code.gson:gson:${LegebuildConst.GSON_GOOGLE}")
+
+  implementation("ch.qos.logback:logback-classic:1.5.17")
+  implementation("ch.qos.logback:logback-core:1.5.17")
+  implementation("org.slf4j:slf4j-api:${LegebuildConst.SLF4J}")
+
+  implementation("org.codejargon:fluentjdbc:1.8.6")
+  implementation("com.zaxxer:HikariCP:7.0.2")
+  // Add more databases as we need to support them
+  implementation("com.microsoft.sqlserver:mssql-jdbc:7.4.1.jre8")
+  implementation("org.antlr:antlr4-runtime:4.13.2")  // For MS SQL Server useFmtOnly feature
+  implementation("mysql:mysql-connector-java:8.0.33") {
+    exclude(group = "com.google.protobuf", module = "protobuf-java")
+  }
+  implementation("org.postgresql:postgresql:42.7.5")
+
+  implementation("org.yaml:snakeyaml:1.33")
+
+  val junitVersion = "6.0.0"
+  testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+  testImplementation("io.wisetime:wisetime-test-support:2.9.53")
+  testImplementation("org.flywaydb:flyway-core:7.15.0")
+  testImplementation("com.github.javafaker:javafaker:1.0.2")
+  testImplementation("org.mockito:mockito-core:5.18.0")
+  testImplementation("org.assertj:assertj-core:3.27.2")
+}
+
+configurations.all {
+  resolutionStrategy {
+    eachDependency {
+      if (requested.group == "com.fasterxml.jackson.core") {
+        useVersion(LegebuildConst.JACKSON_FASTER)
+        because("use consistent version for all transitive dependencies")
+      }
+      if (requested.name == "commons-lang3") {
+        useVersion("3.18.0")
+        because("use consistent version for all transitive dependencies")
+      }
+    }
+    force("org.slf4j:jcl-over-slf4j:${LegebuildConst.SLF4J}")
+    force("joda-time:joda-time:${LegebuildConst.JODA_TIME}")
+  }
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_25
+  targetCompatibility = JavaVersion.VERSION_25
+}
+
+application {
+  mainClass.set("io.wisetime.connector.sql.ConnectorLauncher")
+  applicationDefaultJvmArgs = listOf(
+    "-server",
+    "-Xms128m",
+    "-Xmx256m",
+    "-XX:+UseConcMarkSweepGC",
+    "-XX:MaxMetaspaceSize=256m"
+  )
+}
+
+apply(from = "$rootDir/gradle/jacoco.gradle")
+
+jib {
+  container {
+    mainClass = "io.wisetime.connector.sql.ConnectorLauncher"
+  }
+  from {
+    image = "${project.properties["com.wisetime.jib.baseimage"]}"
+  }
+  to {
+    project.afterEvaluate { // <-- so we evaluate version after it has been set
+      image = "europe-west3-docker.pkg.dev/legebuild/connectors/wisetime-sql-connector:${project.version}"
+    }
+  }
+}
+
+tasks.withType(com.google.cloud.tools.jib.gradle.JibTask::class.java) {
+  dependsOn(tasks.compileJava)
+}
+
+jgitver {
+  autoIncrementPatch(false)
+  strategy(fr.brouillard.oss.jgitver.Strategies.PATTERN)
+  versionPattern("\${meta.CURRENT_VERSION_MAJOR}.\${meta.CURRENT_VERSION_MINOR}.\${meta.COMMIT_DISTANCE}")
+  regexVersionTag("v(\\d+\\.\\d+(\\.0)?)")
+}
+
+apply(from = "$rootDir/gradle/checkstyle.gradle")
+
+tasks {
+  check {
+    dependsOn(jacocoTestCoverageVerification)
+  }
+
+  clean {
+    setDelete(setOf("build", "out"))
+  }
+
+  dependencyUpdates {
+    checkForGradleUpdate = false
+    revision = "release"
+    outputFormatter = "json"
+    reportfileName = "report"
+    outputDir = "$projectDir/build/dependencyUpdates"
+  }
+
+  withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+      listOf("alpha", "beta", "b", "rc", "cr", "m", "preview", "snapshot", "ea")
+        .any { skipItem -> candidate.version.lowercase().contains(skipItem) }
+    }
+  }
+
+  test {
+    testLogging {
+      // skip logging PASSED
+      setEvents(listOf(TestLogEvent.SKIPPED, TestLogEvent.FAILED))
+      exceptionFormat = TestExceptionFormat.FULL
+    }
+
+    useJUnitPlatform {
+      excludeTags = setOf("disabled", "integration", "integration-hq-stage")
+    }
+
+    finalizedBy(jacocoTestReport)
+  }
+}
+
+publishing {
+  repositories {
+    maven {
+      url = uri("s3://artifacts.wisetime.com/mvn2/releases")
+      authentication {
+        register<AwsImAuthentication>("awsIm")
+      }
+    }
+  }
+  publications {
+    register<MavenPublication>("mavenJava") {
+      groupId = "io.wisetime"
+      artifactId = "wisetime-sql-connector"
+      // version is set via plugin
+      from(components["java"])
+    }
+  }
+}
+
+tasks.register<DefaultTask>("printVersionStr") {
+  doLast {
+    println("${project.version}")
+  }
+}
---
+++ src/test/java/io/wisetime/connector/sql/sync/ConnectedDatabaseTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2020 Practice Insight Pty Ltd. All Rights Reserved.
+ */
+
+package io.wisetime.connector.sql.sync;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import com.github.javafaker.Faker;
+import io.vavr.control.Try;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class ConnectedDatabaseTest {
+
+  public static Faker FAKER = Faker.instance();
+
+  @Test
+  void toTagSyncRecord_allDataPresent() throws SQLException {
+    final Map<String, String> dataMap = getTestDataMap();
+    final ResultSet resultSet = createMockResultSet(dataMap);
+
+    final TagSyncRecord tagSyncRecord = TagSyncRecord.fluentJdbcMapper().map(resultSet);
+
+    assertThat(tagSyncRecord.getSyncMarker()).isEqualTo(dataMap.get("sync_marker"));
+    assertThat(tagSyncRecord.getTagDescription()).isEqualTo(dataMap.get("tag_description"));
+    assertThat(tagSyncRecord.getUrl()).isEqualTo(dataMap.get("url"));
+    assertThat(tagSyncRecord.getTagName()).isEqualTo(dataMap.get("tag_name"));
+    assertThat(tagSyncRecord.getTagMetadata()).isEqualTo(dataMap.get("tag_metadata"));
+    assertThat(tagSyncRecord.getId()).isEqualTo(dataMap.get("id"));
+    assertThat(tagSyncRecord.getAdditionalKeyword()).isEqualTo(dataMap.get("additional_keyword"));
+  }
+
+  @Test
+  void toTagSyncRecord_missingOptionalData() throws SQLException {
+    final Map<String, String> dataMap = getTestDataMap();
+    dataMap.remove("tag_metadata");
+    dataMap.remove("url");
+    final ResultSet resultSet = createMockResultSet(dataMap);
+
+    final TagSyncRecord tagSyncRecord = TagSyncRecord.fluentJdbcMapper().map(resultSet);
+
+    assertThat(tagSyncRecord.getSyncMarker()).isEqualTo(dataMap.get("sync_marker"));
+    assertThat(tagSyncRecord.getTagDescription()).isEqualTo(dataMap.get("tag_description"));
+    assertThat(tagSyncRecord.getUrl()).isEqualTo(null);
+    assertThat(tagSyncRecord.getTagName()).isEqualTo(dataMap.get("tag_name"));
+    assertThat(tagSyncRecord.getTagMetadata()).isEqualTo("{}");
+    assertThat(tagSyncRecord.getId()).isEqualTo(dataMap.get("id"));
+    assertThat(tagSyncRecord.getAdditionalKeyword()).isEqualTo(dataMap.get("additional_keyword"));
+  }
+
+  private Map<String, String> getTestDataMap() {
+    Map<String, String> dataMap = Map.of("id", FAKER.idNumber().valid(),
+        "tag_name", FAKER.team().name(),
+        "url", FAKER.internet().url(),
+        "tag_metadata", "{ location='" + FAKER.address().country() + "'}",
+        "additional_keyword", FAKER.company().name(),
+        "tag_description", FAKER.company().industry(),
+        "sync_marker", FAKER.idNumber().valid());
+    return new HashMap<>(dataMap);
+  }
+
+  private ResultSet createMockResultSet(Map<String, String> fields) {
+    ResultSet mockResultSet = mock(ResultSet.class);
+    fields.forEach((key, value) -> Try.of(() -> doReturn(value).when(mockResultSet).getString(key)));
+    return mockResultSet;
+  }
+}
